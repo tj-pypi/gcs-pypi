c2e14e631ecc93c587796d4d194d2cc83fc1dc84
Tonye Jack <tonye@coherentpath.com>
2020-05-12 01:14:17 -0400

    Updated poetry lock.

--- setup.py
+++ setup.py
@@ -34,5 +34,5 @@ setup(
     package_dir={"": "."},
     package_data={"gcspypi": ["templates/*.j2"]},
     install_requires=['futures==3.*,>=3.3.0; python_version == "2.7.*" and python_version >= "2.7.0"', 'google-api-core==1.*,>=1.17.0', 'google-cloud-core==1.*,>=1.3.0', 'google-cloud-storage==1.*,>=1.28.0', 'jinja2==2.*,>=2.10.1', 'wheel==0.*,>=0.33.6'],
-    extras_require={"dev": ["black==19.*,>=19.3.0.b0; python_version == \"3.*\" and python_version >= \"3.6.0\"", "dephell==0.*,>=0.8.3; python_version == \"3.6\"", "flake8==3.*,>=3.7.8", "ipdb==0.*,>=0.13.2", "isort==4.*,>=4.3.21", "pytest>=4.6.5", "pytest-cov==2.*,>=2.7.1"]},
+    extras_require={"dev": ["black==19.*,>=19.3.0.b0; python_version == \"3.*\" and python_version >= \"3.6.0\"", "dephell==0.*,>=0.8.3; python_version == \"3.*\" and python_version >= \"3.6.0\"", "flake8==3.*,>=3.7.8", "ipdb==0.*,>=0.13.2", "isort==4.*,>=4.3.21", "mock==4.*,>=4.0.2; python_version == \"3.*\" and python_version >= \"3.6.0\"", "pytest>=4.6.5", "pytest-cov==2.*,>=2.7.1"]},
 )
